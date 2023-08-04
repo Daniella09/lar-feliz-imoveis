772361c063346e704a1bf5cf570da2bffaf8f052
daniella09 <brigit.daniella.ios@gmail.com>
2023-08-04 17:09:07 -0300

    projeto angular

---
+++ lar-feliz-imoveis/src/app/detalhes/detalhes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-detalhes',
+  templateUrl: './detalhes.component.html',
+  styleUrls: ['./detalhes.component.scss']
+})
+export class DetalhesComponent implements OnInit {
+
+  imovelId: string | undefined;
+
+
+  constructor(private router: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.router.paramMap.subscribe(params => {
+      this.imovelId = params.get('id') ?? '';
+    });
+  }
+
+}
